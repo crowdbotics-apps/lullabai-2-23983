5188d819fc34a6beb73a4d39a1d06190228a6107
Crowdbotics <team@crowdbotics.com>
2021-01-22 05:37:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lullabai_2_23983/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lullabai_2_23983;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "lullabai_2_23983";
+    }
+}
